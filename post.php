079f547df8b13a0cc719bce3b7a51d0394a36f13
eudalio <tonnydallios@gmail.com>
2018-02-24 12:25:26 -0300

    built project structure

---
+++ post.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="./node_modules/materialize-css/dist/css/materialize.min.css"  media="screen,projection"/>
+			
+			<!-- Import css custom -->
+			<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+      
+			<!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+			<nav class="cyan">
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo center">Projeto - Fotolog</a>
+					<ul id="nav-mobile" class="left hide-on-med-and-down">
+						<li class="active"><a href="post.php">Nova postagem</a></li>
+						<li><a href="users.php">Usuários</a></li>
+						<li><a href="fotolog.php">Fotos</a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="main">
+			
+			</div>
+
+			<footer class="page-footer cyan gray-text darken-2-text">
+				<div class="container"></div>
+        <div class="footer-copyright">
+          <div class="container">
+          	© 2018 Copyright Fotolog - Developed by Eudálio Sousa
+          	<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+      </footer>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
+      <script type="text/javascript" src="./node_modules/materialize-css/dist/js/materialize.min.js"></script>
+    </body>
+  </html>
+        
